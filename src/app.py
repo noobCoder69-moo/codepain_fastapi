fdafc37dfcaa3167136f8176f1765bd3676f2edf
Kalpan Talukder <kalpantalukder10@gmail.com>
2025-11-15 23:35:09 +0530

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routes import auth_routes, pen_routes
 
 app = FastAPI()
+
+app.include_router(auth_routes)
+app.include_router(pen_routes)
 
 @app.get('/')
 def root():
