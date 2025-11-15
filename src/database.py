b2aca63c800b8ffebece883d479932c5b64fed46
Kalpan Talukder <kalpantalukder10@gmail.com>
2025-11-16 01:37:30 +0530

    minor env file change

--- src/database.py
+++ src/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DB_URL = 'sqlite:///./pens.db'
+SQLALCHEMY_DB_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DB_URL, connect_args={'check_same_thread' : False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
